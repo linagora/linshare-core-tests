3c36ab0b25fd4eb0b24b0b3e348f5a556773f4f7
Frédéric MARTIN <fmartin@linagora.com>
2021-10-25 17:47:18 +0200

    Adding some exta tests  for functionality api

--- linshare_core_tests/admin/v5/test_functionalities.py
+++ linshare_core_tests/admin/v5/test_functionalities.py
@@ -56,8 +56,8 @@ def test_find_all_functionalites_and_subs(request_helper, base_url):
     assert data
     assert len([a['identifier'] for a in data]) == 45
     assert not DeepDiff(
-        data,
-        load_data_from_file("test_functionalities.all.json")
+        load_data_from_file("test_functionalities.all.json"),
+        data
     )
 
 
@@ -74,6 +74,200 @@ def test_find_functionality_share_expiration(request_helper, base_url):
     log.debug("functionalities: %s", data)
     assert data
     assert not DeepDiff(
-        data,
-        load_data_from_file("test_functionalities.share_expiration.json")
+        load_data_from_file("test_functionalities.share_expiration.json"),
+        data
     )
+
+
+def test_update_functionality_share_expiration_ap(request_helper, base_url):
+    """Test of activation policy updates"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'SHARE_EXPIRATION'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['activationPolicy']['allowOverride']['value'] = False
+    orig['activationPolicy']['enable']['value'] = False
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['activationPolicy']['allowOverride']['overriden'] = True
+    orig['activationPolicy']['enable']['overriden'] = True
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
+
+
+def test_update_functionality_share_expiration_cp_false(
+        request_helper, base_url):
+    """Test of configuration policy updates"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'SHARE_EXPIRATION'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['configurationPolicy']['allowOverride']['value'] = False
+    orig['configurationPolicy']['enable']['value'] = False
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['configurationPolicy']['allowOverride']['overriden'] = True
+    orig['configurationPolicy']['enable']['overriden'] = True
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
+
+
+def test_update_functionality_share_expiration_cp_true(
+        request_helper, base_url):
+    """Test of configuration policy updates"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'SHARE_EXPIRATION'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['configurationPolicy']['allowOverride']['value'] = True
+    orig['configurationPolicy']['enable']['value'] = True
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['configurationPolicy']['allowOverride']['overriden'] = False
+    orig['configurationPolicy']['enable']['overriden'] = False
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
+
+
+def test_update_functionality_share_expiration_dp(request_helper, base_url):
+    """Test of delegation policy updates"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'SHARE_EXPIRATION'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['delegationPolicy']['allowOverride']['value'] = False
+    orig['delegationPolicy']['enable']['value'] = False
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['delegationPolicy']['allowOverride']['overriden'] = True
+    orig['delegationPolicy']['enable']['overriden'] = True
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
+
+
+def test_update_functionality_share_expiration_parameters(
+        request_helper, base_url):
+    """Test of parameters updates"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'SHARE_EXPIRATION'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['parameter']['defaut']['value'] = 6
+    orig['parameter']['defaut']['unit'] = "WEEK"
+    orig['parameter']['maximum']['value'] = 9
+    orig['parameter']['maximum']['unit'] = "WEEK"
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['parameter']['defaut']['parentValue'] = 6
+    orig['parameter']['defaut']['parentUnit'] = "WEEK"
+    orig['parameter']['maximum']['parentValue'] = 9
+    orig['parameter']['maximum']['parentUnit'] = "WEEK"
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
+
+
+def test_find_functionality_upload_request__protected_by_password(
+        request_helper, base_url):
+    """Testing find of a Boolean Functionality"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'UPLOAD_REQUEST__PROTECTED_BY_PASSWORD'
+    })
+    data = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs.test_find_functionality')
+    log.debug("functionalities: %s", data)
+    assert data
+    # before / after
+    assert not DeepDiff(
+        load_data_from_file(
+            "test_functionalities.upload_request__protected_by_password.json"),
+        data
+    )
+
+
+def test_update_functionality_upload_request__protected_by_password(
+        request_helper, base_url):
+    """Testing update of a Boolean Functionality"""
+    query_url = '{baseUrl}/domains/{domain}/functionalities/{identifier}'
+    query_url = query_url.format_map({
+        'domain': "LinShareRootDomain",
+        'baseUrl': base_url,
+        'identifier': 'UPLOAD_REQUEST__PROTECTED_BY_PASSWORD'
+    })
+    orig = request_helper.get(query_url)
+    log = logging.getLogger('tests.funcs')
+    log.debug("functionalities: %s", orig)
+    assert orig
+    # changing some values
+    orig['delegationPolicy']['allowOverride']['value'] = False
+    orig['delegationPolicy']['enable']['value'] = False
+    orig['parameter']['defaut']['value'] = True
+    # sending data to the server
+    output = request_helper.put(query_url, orig)
+    log.debug("output:type:: %s", type(output))
+    assert output
+    # changing the sent payload with what we are expecting.
+    orig['delegationPolicy']['allowOverride']['overriden'] = True
+    orig['delegationPolicy']['enable']['overriden'] = False
+    # Parent value is equal to current value on the root domain.
+    orig['parameter']['defaut']['parentValue'] = True
+    # comparing sent payload with payload sent back by the update method
+    # before / after
+    assert not DeepDiff(orig, output)
