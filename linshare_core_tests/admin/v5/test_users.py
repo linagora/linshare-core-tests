1a4659e16432851fc0cb038fffd4d9ef83784839
Marwan JEBAHI <mjebahi@linagora.com>
2022-01-18 16:58:55 +0100

    Add test fail update for root user

--- linshare_core_tests/admin/v5/test_users.py
+++ linshare_core_tests/admin/v5/test_users.py
@@ -3,31 +3,50 @@
 """Testing users endpoints of adminv5 API."""
 
 
-import urllib
 import logging
-import pytest
 # import json
 
 
-def test_config(admin_cfg, base_url):
-    """Just display current config."""
-    print()
-    print(admin_cfg['DEFAULT']['host'])
-    print(admin_cfg['DEFAULT']['email'])
-    print(admin_cfg['DEFAULT']['password'])
-    print(base_url)
-
-
 def test_find_all(request_helper, base_url):
-    """Test find all users on API v5"""
+    """Test find all users on admin API v5"""
     query_url = '{baseUrl}/users'.format_map({
         'baseUrl': base_url
     })
     users = request_helper.get(query_url)
     log = logging.getLogger('tests.funcs.test_find_all')
     log.debug("users: %s", users)
-    assert users
-    if len(users) != 0:
+    if len(users) > 0:
         for user in users:
             assert user['uuid'] != "root@localhost.localdomain"
 
+
+def test_update_root_user_fail(request_helper, base_url):
+    """Test failing update root user on admin API v5"""
+    query_url = '{baseUrl}/users'.format_map({
+        'baseUrl': base_url
+    })
+    payload = {
+        "uuid": "root@localhost.localdomain",
+        "firstName": "testRootUpdate",
+        "lastName": "administrator",
+        "mail": "root@localhost.localdomain",
+        "role": "SUPERADMIN",
+        "canUpload": True,
+        "canCreateGuest": True,
+        "accountType": "ROOT",
+        "domain":
+            {
+                "uuid": "LinShareRootDomain",
+                "name": "top1"
+            },
+        "externalMailLocale": "ENGLISH",
+        "secondFAUuid": "27dbc951-763c-4d2c-8151-98dec3bc4ee7",
+        "secondFAEnabled": False,
+        "locked": False
+    }
+    request_helper.put(
+        query_url,
+        payload,
+        expected_status=403,
+        busines_err_code=17000
+    )
