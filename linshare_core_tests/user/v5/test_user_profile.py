7ee6fbddb34a2de4aec2da6e558ee870d9ff7677
Antoine DUPRAT <aduprat@linagora.com>
2022-02-16 10:43:34 +0100

    Add Userv5 PUT /me/profile endpoint tests

--- linshare_core_tests/user/v5/test_user_profile.py
+++ linshare_core_tests/user/v5/test_user_profile.py
@@ -53,3 +53,150 @@ def test_find_user_profile_guest(request_helper, base_url, new_guest):
     assert data['locale'] == 'ENGLISH'
     assert not data['personalSpaceEnabled']
     assert data['accountType'] == 'GUEST'
+
+
+def test_update_user_profile_first_name_fail_internal(
+        request_helper, base_url):
+    """Test updating a user profile first name should fail - internal"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(query_url)
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    user['firstName'] = 'Pedro'
+    request_helper.put(query_url, payload=user,
+                       expected_status=400, busines_err_code=67001)
+
+
+def test_update_user_profile_account_type_fail_internal(
+        request_helper, base_url, new_guest):
+    """Test updating a user profile account type should fail - internal"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(query_url)
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    # Convert INTERNAL json into GUEST json
+    del user['canCreateGuest']
+    user['expirationDate'] = new_guest['creationDate']
+    user['restricted'] = new_guest['restricted']
+    user['author'] = new_guest['owner']
+    del user['author']['domain']
+    del user['author']['accountType']
+    del user['author']['external']
+
+    user['accountType'] = 'GUEST'
+    request_helper.put(query_url, payload=user,
+                       expected_status=400, busines_err_code=67000)
+
+
+def test_update_user_profile_last_name_fail_guest(
+        request_helper, base_url, new_guest):
+    """Test updating a user profile last name should fail - guest"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(
+        query_url, email=new_guest['mail'], password='MyGuest@Password123')
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    user['lastName'] = 'Hawkins'
+    request_helper.put(query_url, payload=user,
+                       email=new_guest['mail'], password='MyGuest@Password123',
+                       expected_status=400, busines_err_code=67001)
+
+
+def test_update_user_profile_account_type_fail_guest(
+        request_helper, base_url, new_guest):
+    """Test updating a user profile account type should fail - guest"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(
+        query_url, email=new_guest['mail'], password='MyGuest@Password123')
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    # Convert GUEST json into INTERNAL json
+    user['canCreateGuest'] = True
+    del user['expirationDate']
+    del user['restricted']
+    del user['author']
+    user['canCreateGuest'] = True
+    user['accountType'] = 'INTERNAL'
+    request_helper.put(query_url, payload=user,
+                       email=new_guest['mail'], password='MyGuest@Password123',
+                       expected_status=400, busines_err_code=67000)
+
+
+def test_update_user_profile_locale_internal(
+        request_helper, base_url):
+    """Test updating a user profile locale - internal"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(query_url)
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    user['locale'] = 'RUSSIAN'
+    request_helper.put(query_url, payload=user)
+
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(query_url)
+    assert user['locale'] == 'RUSSIAN'
+
+
+def test_update_user_profile_locale_guest(
+        request_helper, base_url, new_guest):
+    """Test updating a user profile locale - guest"""
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(
+        query_url, email=new_guest['mail'], password='MyGuest@Password123')
+
+    query_url = '{baseUrl}/me/profile/{uuid}'
+    query_url = query_url.format_map({
+        'baseUrl': base_url,
+        'uuid': user['uuid']
+    })
+    user['locale'] = 'RUSSIAN'
+    request_helper.put(query_url, payload=user,
+                       email=new_guest['mail'], password='MyGuest@Password123')
+
+    query_url = '{baseUrl}/me/profile'
+    query_url = query_url.format_map({
+        'baseUrl': base_url
+    })
+    user = request_helper.get(query_url)
+    assert user['locale'] == 'RUSSIAN'
